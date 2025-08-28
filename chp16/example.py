9ba5f8c756a871dda0283eb39a14282ae06461a6
kcw2297 <kcw2371@gmail.com>
2025-08-28 21:24:32 +0900

    feat: chp16 드디어 추상화

---
+++ chp16/example.py
@@ -0,0 +1,86 @@
+from abc import abstractmethod
+
+class Expression:
+    @abstractmethod    
+    def reduce(self, bank: "Bank", to: str) -> 'Money':
+        pass
+    
+    @abstractmethod
+    def plus(self, added: "Expression") -> 'Expression':
+        pass
+    
+    @abstractmethod
+    def times(self, multiplier: int) -> 'Expression':
+        pass
+    
+class Money(Expression):
+    def __init__(self, value: int, currency: str):
+        self.amount = value
+        self._currency = currency
+
+    @staticmethod
+    def dollar(amount: int) -> 'Money':
+        return Money(amount, 'USD')
+
+    @staticmethod
+    def franc(amount: int) -> 'Money':
+        return Money(amount, 'CHF')
+    
+    def currency(self) -> str:
+        return self._currency
+
+    def equals(self, money: 'Money') -> bool:
+        return self.amount == money.amount and self._currency == money.currency()
+
+    def times(self, multiplier: int) -> Expression:
+        return Money(self.amount * multiplier, self._currency)
+    
+    def plus(self, addend: Expression) -> Expression:
+        return Sum(self, addend)
+    
+    def reduce(self, bank: 'Bank',  to: str) -> 'Money':
+        rate = bank.rate(self._currency, to)
+        return Money(self.amount // rate, to)
+    
+class Bank:
+    def __init__(self):
+        self.rates = {}
+    
+    def add_rate(self, source: str, to: str, rate:int) -> None:
+        self.rates[Pair(source, to)] = rate
+    
+    def rate(self, source: str, to: str):
+        if source == to: return 1
+        return self.rates[Pair(source, to)]
+    
+    def reduce(self, source: Expression, to: str) -> Money:
+        return source.reduce(self, to)
+
+    
+class Sum(Expression):
+    def __init__(self, augend: Expression, addend: Expression):
+        self.augend = augend
+        self.addend = addend
+        
+    def reduce(self, bank: "Bank", to: str) -> Money:
+        amount = self.augend.reduce(bank, to).amount + self.addend.reduce(bank, to).amount
+        return Money(amount, to)
+    
+    def plus(self, addend: Expression) -> Expression:
+        return Sum(self, addend)
+    
+    def times(self, multiplier: int) -> Expression:
+        return Sum(self.augend.times(multiplier), self.addend.times(multiplier))
+    
+class Pair():
+    def __init__(self, source: str, to: str):
+        self.source = source
+        self.to = to
+    
+    def __eq__(self, other: "Pair") -> bool:
+        return self.source == other.source and self.to == other.to
+    
+    def __hash__(self) -> int:
+        return hash((self.source, self.to))
+        
+        
---
+++ chp16/test_example.py
@@ -0,0 +1,84 @@
+from example import  Money, Bank, Sum
+
+def test_currency():
+    assert Money.dollar(1).currency() == 'USD'
+    assert Money.franc(1).currency() == 'CHF'
+
+def test_multiplication():
+    dollar_instance = Money.dollar(5)
+    result1 = dollar_instance.times(2)
+    assert isinstance(result1, Money) and Money.dollar(10).equals(result1)
+    result2 = dollar_instance.times(3)
+    assert isinstance(result2, Money) and Money.dollar(15).equals(result2)
+    
+def test_simple_addition():
+    five = Money.dollar(5)
+    sum = five.plus(five)
+    bank = Bank()
+    reduced = bank.reduce(sum, 'USD')
+    
+    assert reduced.amount == Money.dollar(10).amount
+
+def test_plus_return_sum():
+    five = Money.dollar(5)
+    sum = five.plus(five)
+    assert isinstance(sum, Sum)
+    assert five == sum.augend
+    assert five == sum.addend
+    
+def test_reduce_sum():
+    sum = Sum(Money.dollar(3), Money.dollar(4))
+    bank = Bank()
+    result = bank.reduce(sum, "USD")
+    assert Money.dollar(7).amount == result.amount    
+
+def test_reduce_money():
+    bank = Bank()
+    result = bank.reduce(Money.dollar(1), 'USD')
+    assert Money.dollar(1).amount == result.amount
+    
+
+def test_identity_rate():
+    bank = Bank()
+    assert 1 == bank.rate("USD", "USD")
+
+    
+def test_reduce_money_different_currency():
+    bank = Bank()
+    bank.add_rate("CHF", "USD", 2)
+    result = bank.reduce(Money.franc(2), "USD")
+    
+    assert Money.dollar(1).amount == result.amount
+    
+    
+def test_mixed_addition():
+    fiveBucks = Money.dollar(5)
+    tenFrancs = Money.franc(10)
+    
+    bank = Bank()
+    bank.add_rate("CHF", "USD", 2)
+    
+    result = bank.reduce(fiveBucks.plus(tenFrancs), 'USD')
+    
+    assert Money.dollar(10).amount == result.amount
+    
+    
+def test_sum_plus_money():
+    fiveBucks = Money.dollar(5)
+    tenFrances = Money.franc(10)
+    bank = Bank()
+    bank.add_rate("CHF", 'USD', 2)
+    sum = Sum(fiveBucks, tenFrances).plus(fiveBucks)
+    result = bank.reduce(sum, "USD")
+    assert Money.dollar(15).amount == result.amount
+    
+def test_sum_times(): 
+    fiveBucks = Money.dollar(5)
+    tenFrancs = Money.franc(10)
+    bank = Bank()
+    bank.add_rate("CHF", "USD", 2)
+    sum = Sum(fiveBucks, tenFrancs).times(2)
+    result = bank.reduce(sum, "USD")
+    assert Money.dollar(20).amount == result.amount
+    
+    
